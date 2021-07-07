df0fb0f784fe73e2ab370b713b348d33fc7ba264
Ye Luo <yeluo@anl.gov>
2021-07-06 20:14:34 -0500

    Remove generic-simd in fftw

--- var/spack/repos/builtin/packages/fftw/package.py
+++ var/spack/repos/builtin/packages/fftw/package.py
@@ -5,6 +5,7 @@
 
 import os
 import os.path
+
 import llnl.util.lang
 from spack import *
 
@@ -136,16 +137,6 @@ class FftwBase(AutotoolsPackage):
         for feature in simd_features:
             msg = '--enable-{0}' if feature in spec.target else '--disable-{0}'
             simd_options.append(msg.format(feature))
-
-        # If no features are found, enable the generic ones
-        if not any(f in spec.target for f in
-                   simd_features + float_simd_features):
-            # Workaround NVIDIA compiler bug
-            if not spec.satisfies('%nvhpc'):
-                simd_options += [
-                    '--enable-generic-simd128',
-                    '--enable-generic-simd256'
-                ]
 
         simd_options += [
             '--enable-fma' if 'fma' in spec.target else '--disable-fma'
